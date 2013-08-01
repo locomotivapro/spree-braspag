5f866d25663f053983b3ab4a59e8777cc44379c0
Denis Tierno <de.tierno@gmail.com>
2013-08-01 14:34:51 -0300

    removed validation of amount in creditcard

--- app/models/spree/creditcard_transaction.rb
+++ app/models/spree/creditcard_transaction.rb
@@ -2,6 +2,6 @@ module Spree
   class CreditcardTransaction < Spree::BraspagTransaction
     attr_accessible :amount, :transaction_id, :message, :number, :return_code, :status
 
-    validates :amount, :return_code, :status, presence: true
+    validates :return_code, :status, presence: true
   end
 end
